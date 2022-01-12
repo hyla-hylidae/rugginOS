ba37150769fe4f26f503cb695c01b7634315e32d
Zippy Frog <zippy@thejumpingfrog.me>
2022-01-12 01:07:54 +0100

    tests: print to host console via serial port

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,40 @@
 #![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+
+    #[cfg(not(test))]
     printkln!("{}", info);
+
+    #[cfg(test)] {
+        serial_println!("[failed]\n");
+        serial_println!("{}", info);
+        exit_qemu(QemuExitCode::Failed);
+    }
+
     loop{}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 /// Kernel entry point
@@ -29,17 +55,34 @@ pub extern "C" fn _start() -> ! {
     loop{}
 }
 
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    printkln!("Running {} tests", tests.len());
-    for test in tests {
-        test();
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+// blanket implementation
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
     }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    printk!("trivial assertion... ");
+
     assert_eq!(1,1);
-    printkln!("[ok]");
 }
